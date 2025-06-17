bdc12e1a15e75cbb39e6b83351f46e581c8b60d4
The UnderLine <114292928+the-under-line@users.noreply.github.com>
2025-06-17 16:20:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from pyscript import Element
 import random
     
     
@@ -12,23 +13,23 @@ def a_chance_at_victory():
         "Mouse": "+1 point IF you attack, +2 points IF you get attacked or IF you do nothing",
         "Shield": "+6 points IF you get attacked",
     }
-    
-    input("Pull a random weapon from the bag (type 'choose' to choose): ")
+    output = Element("output")
+    # input("Pull a random weapon from the bag (type 'choose' to choose): ")
     weapon_choice = random.choice(list(weapons.items()))
-    print(f"You got a {weapon_choice[0]}! This weapon gives you {weapon_choice[1]}!\n")
+    output.write(f"You got a {weapon_choice[0]}! This weapon gives you {weapon_choice[1]}!\n")
     
     # Dice
-    input("Roll 2 die for your starting points (type 'roll' to roll): ")
+    # input("Roll 2 die for your starting points (type 'roll' to roll): ")
     die_one = random.randint(1,6)
     die_two = random.randint(1,6)
     total = die_one+die_two
-    print(f"You got a {die_one} on your first die and a {die_two} on your second die! This totals to {total}!\n")
+    output.write(f"You got a {die_one} on your first die and a {die_two} on your second die! This totals to {total}!\n")
     
     # Spinner
-    input("Spin a spinner: will you attack, get attacked, or do nothing (type 'spin' to spin)?: ")
+    # input("Spin a spinner: will you attack, get attacked, or do nothing (type 'spin' to spin)?: ")
     spinner = ["Attack!", "Get Attacked :(", "Do Nothing..."]
     action = random.choice(spinner)
-    print(f"You got {action}\n")
+    output.write(f"You got {action}\n")
     
     # Final
     attack_multiplier = 1
@@ -56,15 +57,15 @@ def a_chance_at_victory():
     elif action == "Get Attacked :(":
         points -= 5
     
-    print(f"Game is over! You got {points} points!")
+    output.write(f"Game is over! You got {points} points!")
     
     if points < 0:
-        print("You lose. You beat 0 enemies.")
+        output.write("You lose. You beat 0 enemies.")
     elif points <= 7:
-        print("You beat one enemy.")
+        output.write("You beat one enemy.")
     elif points <= 13:
-        print("You beat two enemies!")
+        output.write("You beat two enemies!")
     elif points <= 19:
-        print("You beat three enemies!!")
+        output.write("You beat three enemies!!")
     elif points >= 20:
-        print("You win! You beat ALL FOUR enemies!!!!")
+        output.write("You win! You beat ALL FOUR enemies!!!!")
